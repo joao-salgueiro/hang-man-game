4948da01571a44f97fde1139de119f1c69f77acb
João Pedro Salgueiro da Siqueira <jp-siqueira@outlook.com>
2025-02-19 14:10:04 -0300

    Adding the multiplayer or solo option in the game start

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 def line_definition(word: str, guessed_letters: set):
     """Reveals guessed letters and hides others with '*'."""
     return ''.join(char if char.lower() in guessed_letters else '*' for char in word)
@@ -12,15 +14,21 @@ guessed_letters = set()
 
 """Start with a welcome message and reveal the number of characters of the random word"""
 print('Welcome to the HangMan Game \n')
-print(f'Your random word has', len(random_word) ,'characters')
+print('Do you want to play solo ? Or maybe you want to type a word for someone to try ? type 1 or 2 - ')
+game_choice = input('type 1 for solo or 2 for multiplayer option ')
 
+match game_choice:
+    case "1":
+        print(f'Your random word has', len(random_word) ,'characters')
+    case "2":
+        random_word = input('Type the random word you want - ')
 
 
 """start the attempts until it gets 15 trys"""
 while attempts > 0:
 
     typed_letter = input('\nPlease put one letter - ').strip().lower()
-
+    os.system('cls')
 
 
     """Check if the typed letter is a valid value"""
@@ -30,7 +38,7 @@ while attempts > 0:
 
     if(typed_letter in guessed_letters):
         print('You already try this letter, please type another one')
-
+        continue
 
     """Check if there is the typed value in the random word"""
     if typed_letter in random_word:
@@ -44,7 +52,7 @@ while attempts > 0:
 
     """Verify if the user already have guessed all the letters in the random word"""  
     if '*' not in masked_word:
-        print("🎉 Congratulations! You guessed the word!")
+        print("Congratulations! You guessed the word!")
         break
 
     attempts -= 1
